bfd8a6f3f8e63a56904d7e37c7fae79190a17be7
Mateo <rayoman01@gmail.com>
2024-11-07 19:12:24 -0300

    a
    a

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Duoc-qr-renovado',
+  webDir: 'www'
+};
+
+export default config;
